762a528c11a5d445bf95e793ad500305cb6fa0e6
AJ MICHAEL <84230323+ajmichael521@users.noreply.github.com>
2022-06-10 11:41:15 +0100

    Update BarkingDog.java

--- src/BarkingDog.java
+++ src/BarkingDog.java
@@ -2,7 +2,8 @@
 
 public class BarkingDog {
     public static void main(String[] args) {
-        System.out.println(shouldWakeUp(true,-1));
+        System.out.println(shouldWakeUp(true,1));
+        //returns True(the dog is barking and also the time is less than 8 as the condition states)
     }
     public static boolean shouldWakeUp(boolean barking,int hourOfDay){
          if(hourOfDay < 0 || hourOfDay > 23){
